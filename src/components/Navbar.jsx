d60e10c140243fca22f3243b76c82f7d74f6b30c
Gavensky <Tadeo.Gavensky@nestle.com>
2023-07-19 14:12:18 -0300

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,24 +4,8 @@ import { motion } from "framer-motion";
 export const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const menu = {
-    closed: {
-      scale: 0,
-      transition: {
-        delay: 0.15,
-      },
-    },
-    open: {
-      scale: 1,
-      transition: {
-        type: "spring",
-        duration: 0.4,
-      },
-    },
-  };
-
   return (
-    <nav className="p-6 border-b-[1px] border-gray-200 w-full relative md:fixed flex items-center justify-between md:justify-around">
+    <nav className="p-6 border-b-[1px] border-gray-200 w-full relative md:fixed flex items-center justify-between md:justify-evenly">
       <h1>LOGO</h1>
 
       <ul className="hidden md:flex items-center gap-8">
@@ -43,37 +27,39 @@ export const Navbar = () => {
         Contact us
       </button>
 
-      <button
-        onClick={() => {
-          setIsOpen(!isOpen);
-        }}
-      >
-        <BiMenuAltRight size={30} className="block md:hidden" />
-      </button>
-
-      <div
-        className={`flex flex-col items-start absolute top-20 ${
-          isOpen ? "left-0" : "left-[100%]"
-        } transition-all duration-200 ease-out  bg-white justify-center w-full  shadow-md md:hidden pl-4 gap-8 py-4`}
-      >
-        <ul className="flex flex-col gap-8  p-4 ">
-          <li className="text-sm cursor-pointer font-semibold  hover:text-green-400 transition-all duration-200 ease-in-out">
-            Home
-          </li>
-          <li className="text-sm cursor-pointer font-semibold  hover:text-green-400 transition-all duration-200 ease-in-out">
-            Our Process
-          </li>
-          <li className="text-sm cursor-pointer font-semibold  hover:text-green-400 transition-all duration-200 ease-in-out">
-            Services
-          </li>
-          <li className="text-sm cursor-pointer font-semibold  hover:text-green-400 transition-all duration-200 ease-in-out">
-            Pricing
-          </li>
-        </ul>
-
-        <button className="px-4 py-2 font-semibold rounded-md bg-green-400 text-white hover:bg-green-700 transition-all duration-200 ease-in-out">
-          Contact us
+      <div className="block md:hidden">
+        <button
+          onClick={() => {
+            setIsOpen(!isOpen);
+          }}
+        >
+          <BiMenuAltRight size={30} className="" />
         </button>
+
+        <div
+          className={`flex flex-col items-start absolute top-20 ${
+            isOpen ? "right-0" : "right-[100%]"
+          } transition-all duration-200 ease-out  bg-white justify-center w-full  shadow-md md:hidden pl-4 gap-8 py-4`}
+        >
+          <ul className="flex flex-col gap-8  p-4 ">
+            <li className="text-sm cursor-pointer font-semibold  hover:text-green-400 transition-all duration-200 ease-in-out">
+              Home
+            </li>
+            <li className="text-sm cursor-pointer font-semibold  hover:text-green-400 transition-all duration-200 ease-in-out">
+              Our Process
+            </li>
+            <li className="text-sm cursor-pointer font-semibold  hover:text-green-400 transition-all duration-200 ease-in-out">
+              Services
+            </li>
+            <li className="text-sm cursor-pointer font-semibold  hover:text-green-400 transition-all duration-200 ease-in-out">
+              Pricing
+            </li>
+          </ul>
+
+          <button className="px-4 py-2 font-semibold rounded-md bg-green-400 text-white hover:bg-green-700 transition-all duration-200 ease-in-out">
+            Contact us
+          </button>
+        </div>
       </div>
     </nav>
   );
