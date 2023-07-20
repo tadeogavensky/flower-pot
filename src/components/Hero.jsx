5418476c5e3220feba764f6b4648d1e868be5265
tadeogavensky <tadeogavensky.email@gmail.com>
2023-07-20 09:40:20 -0300

    sdsd

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,7 @@
-import React from "react";
+import { motion } from "framer-motion";
 
 import rose from "../assets/images/rose.png";
 
-import { motion } from "framer-motion";
 
 import amazon from "../assets/images/amazon.png";
 import meta from "../assets/images/meta.png";
@@ -17,13 +16,17 @@ export const Hero = () => {
         <motion.div
           initial={{ opacity: 0, x: -300 }}
           animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.9 }}
-          className="flex flex-col gap-4 max-w-xl  "
+          transition={{
+            x: { type: "spring", stiffness: 100 },
+          }}
+          className="flex flex-col gap-12 max-w-xl  "
         >
-          <p className="text-4xl md:text-6xl font-bold  text-white tracking-wider ">
-            We will help you grow your <span className="text-[#f64944] leading-relaxed ">flowery</span> projects
+          <p className="text-4xl md:text-6xl font-bold  text-[#FFFFFF] tracking-wider drop-shadow-2xl ">
+            We will help you grow your{" "}
+            <span className="text-[#f64944] leading-relaxed ">flowery</span>{" "}
+            projects
           </p>
-          <p className="text-base">
+          <p className="text-base text-[#333333] drop-shadow-2xl">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore
             reprehenderit in soluta, id ipsum molestiae. Error at ullam
             repellendus, esse labore facilis dolorum animi, commodi molestias
@@ -34,7 +37,6 @@ export const Hero = () => {
         <motion.div
           initial={{ opacity: 0, scale: 0 }}
           animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.9 }}
           className="w-full md:w-1/2"
         >
           <img src={rose} alt="rose" className="object-contain" />
@@ -48,12 +50,14 @@ export const Hero = () => {
         transition={{ duration: 0.3 }}
         className="flex flex-col items-center"
       >
-        <p>We played a key role in the business growth of our clients</p>
+        <p className="text-white font-bold text-lg">
+          We played a key role in the business growth of our clients
+        </p>
         <div>
-          <div className=" mt-4 flex flex-row items-center flex-wrap">
+          <div className=" mt-4 flex flex-row items-center  gap-8">
             {clients.map((img, index) => {
               return (
-                <div className="w-[100px]">
+                <div key={index} className="w-[100px] h-[100px] flex justify-center items-center bg-white rounded-md shadow-lg p-4">
                   <img src={img} alt={index} className="object-cover w-full" />
                 </div>
               );
